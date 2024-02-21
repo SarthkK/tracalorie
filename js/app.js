82b5070bc6576fc0a28345039b74a26a86bbb35e
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-02-21 14:51:25 +0530

    added functionality to add tolocal storage

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ class CalorieTracker {
         this._calorieLimit = Storage.getCalorieLimit();
         this._totalCalories = Storage.getTotalCalories();
         this._meals = Storage.getMeals();
-        this._workouts = [];
+        this._workouts = Storage.getWorkouts();
 
         this._displayCaloriesTotal();
         this._displayCaloriesLimit();
@@ -26,6 +26,7 @@ class CalorieTracker {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
         Storage.updateTotalCalories(this._totalCalories)
+        Storage.saveWorkouts(workout);
         this._displayNewWorkout(workout);
         this._render();
     }
@@ -37,6 +38,7 @@ class CalorieTracker {
             this._totalCalories -= meal.calories;
             Storage.updateTotalCalories(this._totalCalories);
             this._meals.splice(index, 1);
+            Storage.removeMeal(id);
             this._render();
         }
     }
@@ -48,6 +50,7 @@ class CalorieTracker {
             this._totalCalories += workout.calories;
             Storage.updateTotalCalories(this._totalCalories);
             this._workouts.splice(index, 1);
+            Storage.removeWorkout(id);
             this._render();
         }
     }
@@ -56,6 +59,7 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        Storage.clearAll();
         this._render();
     }
 
@@ -67,7 +71,8 @@ class CalorieTracker {
     }
 
     loadItems(){
-        this._meals.forEach(meal => this._displayNewMeal());
+        this._meals.forEach(meal => this._displayNewMeal(meal));
+        this._workouts.forEach(workout => this._displayNewWorkout(workout));
     }
     // Private Methods
     _displayCaloriesTotal(){
@@ -231,14 +236,60 @@ class Storage {
     static saveMeals(meal){
         const meals = Storage.getMeals();
         meals.push(meal);
-        localStorage.setItem('meals', JSON.stringify(meals))
+        localStorage.setItem('meals', JSON.stringify(meals));
+    }
+
+    static getWorkouts(){
+        let workouts;
+        if (localStorage.getItem('workouts') === null){
+            workouts = [];
+        } else {
+            workouts = JSON.parse(localStorage.getItem('workouts'));
+        }
+        return workouts; 
+    }
+
+    static saveWorkouts(workout){
+        const workouts = Storage.getWorkouts();
+        workouts.push(workout);
+        localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static removeWorkout(id){
+        const workouts = Storage.getWorkouts();
+        workouts.forEach( (workout, index) => {
+            if(workout.id === id) {
+                workouts.splice(index, 1);
+            }
+        });
+        localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static removeMeal(id){
+        const meals = Storage.getMeals();
+        meals.forEach( (meal, index) => {
+            if(meal.id === id) {
+                meals.splice(index, 1);
+            }
+        });
+        localStorage.setItem('meals', JSON.stringify(meals));
+    }
+
+    static clearAll(){
+         localStorage.removeItem('totalCalories');
+         localStorage.removeItem('meals');
+         localStorage.removeItem('workouts');
     }
 }
 
 class App {
     constructor(){
         this._tracker = new CalorieTracker();
+        this._loadEventListeners();
+        this._tracker.loadItems();
+    }
 
+    _loadEventListeners(){
         document.getElementById('meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
         document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
         document.getElementById('meal-items').addEventListener('click',  this._removeItems.bind(this, 'meal'));
@@ -247,8 +298,6 @@ class App {
         document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
         document.getElementById('reset').addEventListener('click', this._reset.bind(this));
         document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
-
-        this._tracker.loadItems();
     }
 
     _newItem(type, e){
